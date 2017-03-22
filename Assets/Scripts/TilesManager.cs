dbfcc7253ad5ef1ba15ce3f17500000942e412be
weetman <pnunes91@gmail.com>
2017-03-22 14:15:54 -0300

    working spawns for portals and base
    Implemented portals and base spawning.

--- Assets/Scripts/TilesManager.cs
+++ Assets/Scripts/TilesManager.cs
@@ -19,18 +19,25 @@ public class TilesManager : MonoBehaviour {
             maximum = max;
         }
     }
-    [SerializeField]
-    private Count portalCount = new Count (1, 5);
 
     [SerializeField]
-    private GameObject[] portalTiles;
+    private GameObject portalTile;
+    [SerializeField]
+    private GameObject baseTile;
     [SerializeField]
     private GameObject floorTile;
     [SerializeField]
     private GameObject wallTile;
+    [SerializeField]
+    private List<Vector3> portalPositions = new List<Vector3> ();
+    [SerializeField]
+    private List<Vector3> basePositions = new List<Vector3> ();
+
 
     private Transform tilesHolder;
-    private List<Vector3> gridPositions = new List<Vector3> ();
+    private List<Vector3> innerGridPositions = new List<Vector3> ();
+    private List<Vector3> outerGridPositions = new List<Vector3> ();
+
 
     private void Start () {
         InitizalizeGrid ();
@@ -38,12 +45,16 @@ public class TilesManager : MonoBehaviour {
     }
 
     private void InitizalizeGrid () {
-        gridPositions.Clear ();
-
-        for (int x = 1 ; x < COLUMNS ; x++) {
-            for (int y = 1 ; y < ROWS ; y++) {
-                gridPositions.Add (new Vector3 (x, y, 0f));
-
+        innerGridPositions.Clear ();
+        outerGridPositions.Clear ();
+        //gridPosition gets only the cells that are not at the edge of the screen
+        for (int x = 0 ; x <= COLUMNS ; x++) {
+            for (int y = 0 ; y <= ROWS ; y++) {
+                if (x == 0 || x == COLUMNS || y == 0 || y == ROWS) {
+                    outerGridPositions.Add (new Vector3 (x, y, 0f));
+                    continue;
+                }
+                innerGridPositions.Add (new Vector3 (x, y, 0f));
             }
         }
     }
@@ -51,24 +62,36 @@ public class TilesManager : MonoBehaviour {
     private void MapSetup () {
         tilesHolder = new GameObject ("Board").transform;
         FillGrid ();
-        
+        FillOuterWall ();
     }
 
     private void FillGrid () {
-        for (int x = 0 ; x <= COLUMNS ; x++) {
-            for (int y = 0 ; y <= ROWS ; y++) {
-                GameObject toInstantiate = floorTile;
-                if (x == 0 || x == COLUMNS || y == 0 || y == ROWS) { //build outer walls
-                    toInstantiate = wallTile;
-                }
-
-                if (toInstantiate == null) {
-                    print ("grid toInstantiate gameobject null.");
-                    continue;
-                }
-                GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity, tilesHolder) as GameObject;
-   
+        for (int gridIndex = 0 ; gridIndex < innerGridPositions.Count ; gridIndex++) { //loop through the grid
+            GameObject toInstantiate = floorTile;
+            if (toInstantiate == null) {
+                Debug.LogError ("FillGrid function has nothing to instantiate.");
+                continue;
             }
+            GameObject instance = Instantiate (toInstantiate, innerGridPositions[gridIndex], Quaternion.identity, tilesHolder) as GameObject;
+        }
+    }
+
+    private void FillOuterWall () {
+        for (int gridIndex = 0 ; gridIndex < outerGridPositions.Count ; gridIndex++) { //loop through the outerWallgrid
+            GameObject toInstantiate = wallTile;
+            for (int portalIndex = 0 ; portalIndex < portalPositions.Count ; portalIndex++) { //spawn portals
+                if (outerGridPositions[gridIndex] == portalPositions[portalIndex]) {
+                    toInstantiate = portalTile;
+                    break;
+                }
+            }
+            for (int baseIndex = 0 ; baseIndex < basePositions.Count ; baseIndex++) { //spawn base
+                if (outerGridPositions[gridIndex] == basePositions[baseIndex]) {
+                    toInstantiate = baseTile;
+                    break;
+                }
+            }
+            GameObject instance = Instantiate (toInstantiate, outerGridPositions[gridIndex], Quaternion.identity, tilesHolder) as GameObject;
         }
     }
 
