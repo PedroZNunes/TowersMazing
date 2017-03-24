f6b3f1ed031a28f0ec07c9cb1d918d9ee71cc1eb
weetman <pnunes91@gmail.com>
2017-03-24 20:59:03 -0300

    Moved Scripts
    moved some scripts. created the simplest main menu with an async loader
    with % progress in the console (that is useless because the map loads
    too quickly atm)

---
+++ Assets/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    AsyncOperation levelAsyncLoad;
+    public void StartGame () {
+        levelAsyncLoad = SceneManager.LoadSceneAsync ("Map_01", LoadSceneMode.Single);
+        StartCoroutine (TrackProgress ());
+    }
+
+    IEnumerator TrackProgress () {
+        while (!levelAsyncLoad.isDone) { 
+            print (levelAsyncLoad.progress*100 + "%");
+            yield return null;
+        }
+        print ("done");
+    }
+
+}
